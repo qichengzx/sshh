d842e9a6815e30b744304648ef9531bb5559bbe4
qichengzx <qichengzx@gmail.com>
2018-04-13 14:29:07 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+package=sshh
+version="0.1.2"
+
+platforms=("darwin/amd64" "linux/amd64" "linux/arm" "linux/386")
+
+echo "build..."
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+
+    output_name="${package}-${GOOS}-${GOARCH}_${version}"
+
+    echo "build ${output_name}"
+
+    CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -o "./releases/${output_name}/${package}" main.go
+
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+
+    cd ./releases
+
+    zip -r -m "./${output_name}.zip" "./${output_name}"
+
+    cd ../
+
+done
+
+echo "build done"
